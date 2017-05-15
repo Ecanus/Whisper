32793bd6f96b9321274a63badb1ef91a2fa325f9
Hussain Hassam <hussain.hassam21@gmail.com>
2017-05-15 14:57:02 -0400

    add consistency to graphics on different devices

---
+++ TopDownShooter/Assets/Scripts/Camera/ResolutionAdapation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionAdapation : MonoBehaviour {
+
+    public float orthographicSize = 1;
+    public float aspect = 1.6f;
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(
+            -orthographicSize * aspect,
+            orthographicSize * aspect,
+            -orthographicSize,
+            orthographicSize,
+            Camera.main.nearClipPlane, Camera.main.farClipPlane);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
